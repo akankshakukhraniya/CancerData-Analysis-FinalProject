b9dea3e7815d8415a2f28b6485419b263d7143d2
Luis E. Rojas S <luis.e.rojas.s@gmail.com>
2021-06-08 19:31:20 -0400

    js file

--- static/js/app-survival-rate.js
+++ static/js/app-survival-rate.js
@@ -27,15 +27,15 @@ function runCalculation() {
     // Select the input elements 
     var input_race_origin = d3.select("#race_origin").property("value");
     console.log(input_race_origin);
-
+    var URL;
     if (input_race_origin) {
         var input_survival_months = d3.select("#survival_months").property("value");
         var input_tumour_classification= d3.select("#tumour_classification").property("value");
         var input_tumor_size = d3.select("#tumor_size").property("value");
-        var URL = "https://cancer-data.herokuapp.com/" + input_race_origin + "/" + input_survival_months + "/" + input_tumour_classification + "/" + input_tumor_size;
+        URL = "https://cancer-data.herokuapp.com/" + input_race_origin + "/" + input_survival_months + "/" + input_tumour_classification + "/" + input_tumor_size;
     }
     else {
-        var URL = "https://cancer-data.herokuapp.com/0/0/0/0";
+        URL = "https://cancer-data.herokuapp.com/0/0/0/0";
     }
     console.log(URL);
    
